0fa5e20d5019f8729eeaf535eabc2b0bae47a6b8
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2020-02-01 15:12:33 +0100

    Updates

---
+++ precache-manifest.4be12f7bf0eafcfc177bb18fdf26914b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb57bb52696c2f5abf7868882bb59734",
+    "url": "/zadanie-r/index.html"
+  },
+  {
+    "revision": "1f904b5b74dafe34d34f",
+    "url": "/zadanie-r/static/css/main.29ed31c5.chunk.css"
+  },
+  {
+    "revision": "538a2f1dc6a459c561a3",
+    "url": "/zadanie-r/static/js/2.c1389fbb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zadanie-r/static/js/2.c1389fbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f904b5b74dafe34d34f",
+    "url": "/zadanie-r/static/js/main.bd009e4a.chunk.js"
+  },
+  {
+    "revision": "e0acdbfaeeb09b8ae891",
+    "url": "/zadanie-r/static/js/runtime-main.1aaeb1a3.js"
+  }
+]);
